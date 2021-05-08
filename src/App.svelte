9ec37dd32306a37040ba32a523c18776f54e3a37
Fabio-Bedini <io@ilwebdifabio.it>
2021-05-08 12:43:36 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<nav class="bg-blue-900 shadow-lg">
+  <div class="container mx-auto">
+    <div class="sm:flex">
+      <a href="#" class="text-white text-3xl font-bold p-3">APP LOGO{name}</a>
+      
+      <!-- Menus -->
+      <div class="ml-55 mt-4">
+        <ul class="text-white sm:self-center text-xl">
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">About</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Services</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Blog</a>
+          </li>
+          <li class="sm:inline-block">
+            <a href="#" class="p-3 hover:text-red-900">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</nav>
+
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+};
